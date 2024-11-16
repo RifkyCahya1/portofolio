bac51ff9cf79bd3f92d70d3f6120fe40a154dacd
RifkyCahya1 <rifkyp234@gmail.com>
2024-11-16 20:38:13 +0700

    New

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -20,7 +20,7 @@
     <!-- Styles / Scripts -->
 
     @if (env('APP_ENV') === 'production')
-        <link rel="stylesheet" href="{{ asset('build/assets/app-CqRWb9P8.css') }}">
+        <link rel="stylesheet" href="{{ asset('build/assets/app-C2Y4oB_v.css') }}">
     @else
         @vite('resources/css/app.css')
     @endif
